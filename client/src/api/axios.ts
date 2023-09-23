a9c2296fed2625a6bfccf014396d0dede11abad9
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-09-23 14:51:47 -0400

    routes to products

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://goodshopbackend-production.up.railway.app/api",
+  baseURL: "https://goodshopbackend.up.railway.app/api",
   // baseURL: "http://localhost:3001/api",
   
   headers: {
